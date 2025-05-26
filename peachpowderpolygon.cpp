ca7fdd76c084e672aa7a0e9e5c63905a70ebedf3
no-im-bk <80921911+no-im-bk@users.noreply.github.com>
2025-05-25 18:52:57 -0600

    completed peachpowderpolygon

---
+++ peachpowderpolygon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    if(n % 4 == 0) {
+        printf("No\n");
+    } else {
+        printf("Yes\n");
+    }
+}
